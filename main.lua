908405ddad1fbbe616366432a724308d051ae19a
Casry <84832193+NotCasry@users.noreply.github.com>
2023-03-24 21:24:13 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,22 +1,5 @@
 local module = {}
 
-local min = 1
-local max = 200000
-
--- Billboard
-_G.AlwaysOnTop = true
-_G.MaxDistance = 32
-_G.BillboardSize = UDim2.new(5,0,1,0)
-_G.SizeOffset = Vector2.new(0, 0)
-_G.StudsOffset = Vector3.new(0, 0, 0)
--- Text
-_G.Text_BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-_G.Text_BackgroundTransparency = 0
-_G.Text_BorderColor3 = Color3.fromRGB(0, 0, 0)
-_G.Text_BorderMode = Enum.BorderMode.Outline
-_G.Text_BorderSizePixel = 0
-_G.Text_TextDisplay = ""
-
 function module.Create(Parent)
     local ui = {}
 
