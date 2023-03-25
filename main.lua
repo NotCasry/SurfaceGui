ac3e5c584ff886108270c494d64bdc693c710cbc
Casry <84832193+NotCasry@users.noreply.github.com>
2023-03-24 21:23:38 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,50 +3,40 @@ local module = {}
 local min = 1
 local max = 200000
 
-_G.X = 500
-_G.Y = 500
+-- Billboard
 _G.AlwaysOnTop = true
-_G.Frame_Transparency = 1
-_G.Frame_Color3 = Color3.fromRGB(0, 0, 0)
+_G.MaxDistance = 32
+_G.BillboardSize = UDim2.new(5,0,1,0)
+_G.SizeOffset = Vector2.new(0, 0)
+_G.StudsOffset = Vector3.new(0, 0, 0)
+-- Text
+_G.Text_BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+_G.Text_BackgroundTransparency = 0
+_G.Text_BorderColor3 = Color3.fromRGB(0, 0, 0)
+_G.Text_BorderMode = Enum.BorderMode.Outline
+_G.Text_BorderSizePixel = 0
+_G.Text_TextDisplay = ""
 
 function module.Create(Parent)
     local ui = {}
 
-    local Surface = Instance.new("SurfaceGui")
-    Surface.Parent = Parent
-    Surface.Adornee = Parent
-    Surface.Name = tostring(
-        math.random(min, max)
-    )
-    Surface.CanvasSize = Vector2.new(_G.X, _G.Y)
-    Surface.AlwaysOnTop = _G.AlwaysOnTop
+    local BillboardGui = Instance.new("BillboardGui", Parent)
+    BillboardGui.Adornee = Parent
+    BillboardGui.AlwaysOnTop = _G.AlwaysOnTop
+    BillboardGui.MaxDistance = _G.MaxDistance
+    BillboardGui.Size = _G.BillboardSize
+    BillboardGui.SizeOffset = _G.SizeOffset
+    BillboardGui.StudsOffset = _G.StudsOffset
 
-    local Frame = Instance.new("Frame")
-    Frame.Parent = Surface
-    Frame.Size = UDim2.new(1, 0, 1, 0)
-    Frame.BackgroundTransparency = _G.Frame_Transparency
-    Frame.BackgroundColor3 = _G.Frame_Color3
-
-    function ui.Text()
-        local self = {}
-        self.Font = Enum.Font.Ubuntu
-        self.Text = ""
-        self.TextColor3 = Color3.fromRGB(255, 255, 255)
-        self.TextScaled = true
-        self.TextSize = 15
-        self.TextStrokeColor3 = Color3.fromRGB(0, 0, 0)
-        self.TextStrokeTransparency = 0.5
-
-        local TextLabel = Instance.new("TextLabel", Frame)
-        TextLabel.Font = self.Font
-        TextLabel.Text = self.Text
-        TextLabel.TextColor3 = self.TextColor3
-        TextLabel.TextScaled = self.TextScaled
-        TextLabel.TextSize = self.TextSize
-        TextLabel.TextStrokeColor3 = self.TextStrokeColor3
-        TextLabel.TextStrokeTransparency = self.TextStrokeTransparency
-
-        return self 
+    function ui:CreateText()
+        local TextLabel = Instance.new("TextLabel", BillboardGui)
+        TextLabel.BackgroundColor3 = _G.Text_BackgroundColor3
+        TextLabel.BackgroundTransparency = _G.Text_BackgroundTransparency
+        TextLabel.BorderColor3 = _G.Text_BorderColor3
+        TextLabel.BorderMode = _G.Text_BorderMode
+        TextLabel.BorderSizePixel = _G.Text_BorderSizePixel
+        TextLabel.Text = _G.Text_TextDisplay
+        TextLabel.Size = UDim2.new(1, 0, 1, 0)
     end
 
     return ui
